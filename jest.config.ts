ebb7f51a1bd9ab50034cfeed0a3deb327e30128f
vishvas patel <vishvaspatel1410@gmail.com>
2024-08-31 16:07:30 +0530

    updating jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -3,11 +3,33 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest', // Use ts-jest for TypeScript support
   testEnvironment: 'node', // Use Node.js environment for testing
-  roots: ['<rootDir>/src'], // Directory where your tests are located
-  testMatch: ['**/?(*.)+(spec|test).ts'], // Match test files
-  collectCoverage: true, // Enable code coverage
-  collectCoverageFrom: ['src/**/*.ts'], // Specify which files to collect coverage from
-  coverageReporters: ['text', 'lcov'], // Coverage report formats
+
+   setupFilesAfterEnv: ['<rootDir>/config/db.ts'],
+
+  // Define the root directories for test files and source files
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+
+  // Match test files specifically located in the 'tests' folder
+  testMatch: ['<rootDir>/tests/**/*.(spec|test).ts'],
+
+  // Enable code coverage
+  collectCoverage: true,
+  
+  maxWorkers:1,
+
+  testTimeout:10000,
+  // Collect coverage from 'src' and 'model' folders, excluding 'dist'
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '<rootDir>/model/**/*.ts',
+    '!<rootDir>/dist/**',
+  ],
+
+  // Specify the formats for coverage reports
+  coverageReporters: ['text', 'lcov'],
+
+  // Ignore 'dist' folder from test discovery
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
 
 export default config;
